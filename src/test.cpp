81597e478273ce40044cc3abd128ad4139857a68
Melona <kwonjin@kaist.ac.kr>
2021-12-30 21:41:55 +0900

    Create test.cpp

---
+++ src/test.cpp
@@ -0,0 +1,50 @@
+#include <ros/ros.h>
+#include <std_msgs/Bool.h>
+
+class Sub_and_Pub
+{
+public:
+  Sub_and_Pub()
+  {
+    pub_ = nh_.advertise<std_msgs::Bool>("pick_success" , 100);
+    sub_ = nh_.subscribe("toggle_bt" , 100 , &Sub_and_Pub::btCallback , this);
+    IN_ACTION.data = true;
+    SUCCESS.data = false;
+  }
+
+  void btCallback(const std_msgs::Bool& toggle_bt)
+  {
+    ROS_INFO("receive msg = %d" , toggle_bt.data);
+
+    if(toggle_bt.data)
+    {
+      ROS_INFO("Toggle Button is ON !");
+      pub_.publish(IN_ACTION);
+      ros::Duration(5.0).sleep();
+      ROS_INFO("Now System is Ready !");
+      ros::Duration(1.0).sleep();
+      pub_.publish(SUCCESS);
+    }
+    else
+    {
+      pub_.publish(SUCCESS);
+    }
+  }
+
+private:
+  ros::NodeHandle nh_;
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
+
+  std_msgs::Bool SUCCESS;
+  std_msgs::Bool IN_ACTION;
+
+};
+
+int main(int argc , char** argv)
+{
+  ros::init(argc , argv , "manipulator_test");
+  ROS_INFO("Now System is Ready !");
+  Sub_and_Pub Success_and_Action;
+  ros::spin();
+}
